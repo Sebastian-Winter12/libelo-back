bd9de3d868800b628841dea1e581a95849338a59
marianobura <marianoobura@gmail.com>
2024-10-13 00:12:33 -0300

    Update dependencies and configuration files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ["public/index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateRows: {
+        body: "60px 1fr",
+      },
+    },
   },
   plugins: [],
-}
+};
