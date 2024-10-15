584d2faedd3f8e04a80e75c36522dd8a381a3bef
marianobura <marianoobura@gmail.com>
2024-10-15 00:30:58 -0300

    Fix indentation in App.vue, BaseBody.vue and HomeNav.vue

--- src/components/BaseBody.vue
+++ src/components/BaseBody.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="h-full grid grid-rows-body bg-neutral-100">
-    <slot />
-  </div>
+    <div class="h-full grid grid-rows-body bg-neutral-100">
+        <slot />
+    </div>
 </template>
