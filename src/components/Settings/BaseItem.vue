c3833e796d8604cf4fe8c1f48b9cc00ad4a6e9d2
marianobura <marianoobura@gmail.com>
2024-10-18 23:52:08 -0300

    Refactor settings view and base item component

--- src/components/Settings/BaseItem.vue
+++ src/components/Settings/BaseItem.vue
@@ -1,23 +1,34 @@
 <script setup>
-import { defineProps } from 'vue'
+import { ChevronRight } from 'lucide-vue-next';
+import { defineProps } from 'vue';
 
 defineProps({
     title: String,
-    icon: Object
-})
+    icon: Object,
+    account: {
+        type: Boolean,
+        default: false
+    },
+});
 </script>
 
 <template>
-    <div class="flex flex-col">
-        <h2 class="uppercase py-2 px-3 font-semibold text-sm"></h2>
-        <div class="flex items-center justify-between bg-white w-full p-3 rounded-xl">
-            <div class="flex gap-2 items-center">
+    <div class="flex items-center justify-between bg-white w-full p-3 rounded-xl">
+        <div class="flex gap-2 items-center">
+            <template v-if="account">
+                <img src="https://avatar.iran.liara.run/public/2" alt="Imagen de perfil" class="size-12">
+                <div class="flex flex-col">
+                    <p class="font-semibold">Mariano Buranits</p>
+                    <p class="text-sm text-neutral-500">mariano.buranits@davinci.edu.ar</p>
+                </div>
+            </template>
+            <template v-else>
                 <div class="flex items-center justify-center bg-neutral-200/60 size-10 rounded-lg">
                     <component :is="icon" :size="20" />
                 </div>
-                <p>{{ title }}</p>
-            </div>
-            <ChevronRight :size="24" />
+            </template>
+            <p>{{ title }}</p>
         </div>
+        <ChevronRight :size="24" />
     </div>
 </template>
--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -1,38 +1,30 @@
 <script setup>
 import BaseBody from "@/components/BaseBody.vue";
 import BaseNav from "@/components/BaseNav.vue";
-import { ChevronRight, Book } from "lucide-vue-next";
+import LabelTitle from "@/components/Settings/LabelTitle.vue";
+import BaseItem from "@/components/Settings/BaseItem.vue";
+import { Book } from "lucide-vue-next";
 </script>
 
 <template>
     <BaseBody>
         <BaseNav title="Configuración" />
         <div class="flex flex-col gap-4 p-2">
-            <div class="flex flex-col">
-                <h2 class="uppercase py-2 px-3 font-semibold text-sm">Perfil</h2>
-                <div class="flex items-center justify-between bg-white w-full p-3 rounded-xl">
-                    <div class="flex gap-2 items-center">
-                        <img src="https://avatar.iran.liara.run/public/2" alt="Imagen de perfil" class="size-12">
-                        <div class="flex flex-col">
-                            <p class="font-semibold">Mariano Buranits</p>
-                            <p class="text-sm text-neutral-500">mariano.buranits@davinci.edu.ar</p>
-                        </div>
-                    </div>
-                    <ChevronRight :size="24" />
-                </div>
-            </div>
-            <div class="flex flex-col">
-                <h2 class="uppercase py-2 px-3 font-semibold text-sm">Aprendizaje</h2>
-                <div class="flex items-center justify-between bg-white w-full p-3 rounded-xl">
-                    <div class="flex gap-2 items-center">
-                        <div class="flex items-center justify-center bg-neutral-200/60 size-10 rounded-lg">
-                            <Book :size="20" />
-                        </div>
-                        <p>Materias</p>
-                    </div>
-                    <ChevronRight :size="24" />
-                </div>
-            </div>
+            <LabelTitle label="Perfil">
+                <BaseItem account />
+            </LabelTitle>
+            <LabelTitle label="Aprendizaje">
+                <BaseItem title="Materias" :icon="Book" />
+                <BaseItem title="Objetivos" :icon="Book" />
+                <BaseItem title="Calendario" :icon="Book" />
+                <BaseItem title="Evaluaciones" :icon="Book" />
+                <BaseItem title="Ejercicios" :icon="Book" />
+            </LabelTitle>
+            <LabelTitle label="General">
+                <BaseItem title="Notificaciones" :icon="Book" />
+                <BaseItem title="Idioma" :icon="Book" />
+                <BaseItem title="Personalización" :icon="Book" />
+            </LabelTitle>
         </div>
     </BaseBody>
 </template>
