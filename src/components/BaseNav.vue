13dea667bb55e521d7360de99514047763e287c6
marianobura <marianoobura@gmail.com>
2024-10-18 23:32:22 -0300

    Update BaseNav.vue to dynamically display the title

--- src/components/BaseNav.vue
+++ src/components/BaseNav.vue
@@ -1,6 +1,11 @@
 <script setup>
 import { ArrowLeft, EllipsisVertical } from 'lucide-vue-next';
 import { useRouter } from 'vue-router';
+import { defineProps } from 'vue';
+
+defineProps({
+    title: String
+});
 
 const router = useRouter();
 const goBack = () => router.back();
@@ -11,7 +16,7 @@ const goBack = () => router.back();
         <div @click="goBack" class="size-10 flex items-center justify-center">
             <ArrowLeft :size="20" />
         </div>
-        <div class="font-semibold text-lg">Materia</div>
+        <div class="font-semibold text-lg">{{ title }}</div>
         <div class="size-10 flex items-center justify-center">
             <EllipsisVertical :size="20" />
         </div>
