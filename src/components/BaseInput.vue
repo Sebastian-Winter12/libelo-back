3b2f0449918aec94e174a455ff81291d758d2b47
marianobura <marianoobura@gmail.com>
2024-10-27 20:33:29 -0300

    Refactor BaseInput and add LoginView

--- src/components/BaseInput.vue
+++ src/components/BaseInput.vue
@@ -13,8 +13,8 @@ defineProps({
 </script>
 
 <template>
-    <div class="flex flex-col w-full">
-        <label class="text-gray-700 font-semibold" :for="identifier">{{ label }}</label>
-        <input class="w-full bg-gray-200 text-gray-700 border-2 border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-indigo-500" :name="identifier" :id="identifier" :type="type" :placeholder="placeholder">
+    <div class="flex flex-col gap-1 w-full">
+        <label class="font-semibold" :for="identifier">{{ label }}</label>
+        <input class="w-full bg-gray-200 text-gray-700 border border-gray-300 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-indigo-500" :name="identifier" :id="identifier" :type="type" :placeholder="placeholder">
     </div>
 </template>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,17 @@
+<script setup>
+import BaseInput from '../components/BaseInput.vue';
+</script>
+
+<template>
+    <div class="flex items-center justify-center text-white font-semibold text-3xl w-full h-28 bg-gradient-to-b from-libelo-600 to-libelo-500 rounded-br-xl rounded-bl-xl">Libelo</div>
+    <div class="p-2">
+        <div class="flex flex-col mt-2 mb-6">
+            <h1 class="font-semibold text-2xl">Iniciar sesión</h1>
+            <p class="text-neutral-700">Bienvenido de nuevo a EstudIA</p>
+        </div>
+        <div class="flex flex-col gap-4">
+            <BaseInput identifier="email" placeholder="Introduzca su correo electrónico" label="Correo electrónico" type="text" />
+            <BaseInput identifier="password" placeholder="Introduzca su contraseña" label="Contraseña" type="password" />
+        </div>
+    </div>
+</template>
